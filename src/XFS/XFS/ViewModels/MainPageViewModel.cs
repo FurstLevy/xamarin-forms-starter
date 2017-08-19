00f21a5212feff671351befa785e98f12af27ade
Levy Furst Neto <furst_levy@hotmail.com>
2017-08-19 13:01:27 -0300

    inicio do projeto

---
+++ src/XFS/XFS/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFS.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+
+        public override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
